6b8a167863b08b0a7b7a4e214431ba1ec57edc71
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-01 21:19:06 +0200

    dirbo reiki input rasyk document.querySelector(".ugi input")

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const nameDOM = document.querySelector(".name");
-const ugiDOM = document.querySelector(".ugi");
-const svoriDOM = document.querySelector(".svori");
+const ugiDOM = document.querySelector(".ugi input");
+const svoriDOM = document.querySelector(".svori input");
 const table2DOM = document.querySelector(".table2");
-const kmi = document.getElementById(".kmi")
+const kmiDOM = document.querySelector(".kmi");
 const btnscaicioDOM = document.querySelector(".btnscaicio");
 const btnclearDOM = document.querySelector(".clear");
 
-let HTML = ' ';
 function countKmi() {
-const names = nameDOM.value;
-const svori = parseFloat(svoriDOM.value);
-const ugi = parseFloat(ugiDOM.value);
-const result = svori / ugi ;
-
+    const ugi = parseFloat(ugiDOM.value);
+    const svori = parseFloat(svoriDOM.value);
+    
+    let result = (svori / (ugi * ugi))*10000;
+        
     let kmiClass = ' ';
            
     if(result > 0 && result < 18.5){
@@ -21,25 +19,25 @@ const result = svori / ugi ;
     if(result > 18.5 && result < 25){
         kmiClass = "green";
     }
-    if(result > 0 && result < 18.5){
-        kmiClass = "red";
-    }
-    if(result > 18.5 && result < 25){
+    if(result > 25 && result < 30){
         kmiClass = "blue";
+    }
+    if(result > 30 && result < 43){
+        kmiClass = "red";
     }
    
     let HTML =  `<div id = "kmi" class="kmi ${kmiClass}">
-                <p>KMI ${names} yra ${result}.</p>    
+                <p>KMI yra ${result}.</p>    
             </div>`;
             table2DOM.innerHTML = HTML; 
 }
 
+btnscaicioDOM.addEventListener("click", countKmi);
 
-
-btnscaicioDOM.addEventListener("click", countKmi)
-
-
-const clear = () => table2DOM.innerHTML = `<div class="kmi">  </div>`;
+const clear = () => {
+    table2DOM.innerHTML = `<div class="kmi"></div>`;
+    
+};
 btnclearDOM.addEventListener("click", clear);
 
 
