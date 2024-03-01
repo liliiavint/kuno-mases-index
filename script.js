26b6966421d793fd86b6bd19c5f8cf547e571fa0
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-01 17:08:50 +0200

    up

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 const nameDOM = document.querySelector(".name");
 const ugiDOM = document.querySelector(".ugi");
 const svoriDOM = document.querySelector(".svori");
-const btnscaicioDOM = document.querySelector(".btnscaicio");
-const kmiDOM = document.querySelector(".kmi");
 const table2DOM = document.querySelector(".table2");
+const kmi = document.getElementById(".kmi")
+const btnscaicioDOM = document.querySelector(".btnscaicio");
 const btnclearDOM = document.querySelector(".clear");
-let HTML = '';
+
+let HTML = ' ';
 function countKmi() {
-    const names = nameDOM.value;
-    const svori = parseFloat(svoriDOM.value);
-    const ugi = parseFloat(ugiDOM.value);
-    const result = svori / Math.pow(ugi / 100, 2);
-    if(result > 33){
-        table2DOM.innerHTML = `
-        <div class="kmi red">
-            <p>KMI ${names} yra ${result.toFixed(2)}.</p>    
-            
-        </div>`
+const names = nameDOM.value;
+const svori = parseFloat(svoriDOM.value);
+const ugi = parseFloat(ugiDOM.value);
+const result = svori / ugi ;
+
+    let kmiClass = ' ';
+           
+    if(result > 0 && result < 18.5){
+        kmiClass = "violet";
     }
-    
-  
-    table2DOM.innerHTML = `
-    <div class="kmi green">
-        <p>KMI ${names} yra ${result.toFixed(2)}.</p>    
-       
-    </div>`
+    if(result > 18.5 && result < 25){
+        kmiClass = "green";
+    }
+    if(result > 0 && result < 18.5){
+        kmiClass = "red";
+    }
+    if(result > 18.5 && result < 25){
+        kmiClass = "blue";
+    }
+   
+    let HTML =  `<div id = "kmi" class="kmi ${kmiClass}">
+                <p>KMI ${names} yra ${result}.</p>    
+            </div>`;
+            table2DOM.innerHTML = HTML; 
 }
 
 
-btnscaicioDOM.addEventListener("click", function(){
-countKmi()
 
-    
-       
-    })
-const clear = ()=> table2DOM.innerHTML = `<div class="kmi">  </div>`;
+btnscaicioDOM.addEventListener("click", countKmi)
 
+
+const clear = () => table2DOM.innerHTML = `<div class="kmi">  </div>`;
 btnclearDOM.addEventListener("click", clear);
 
 
