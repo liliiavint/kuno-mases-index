849fbf474df2b68d252afe95e1589c2354e6227e
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-02 16:51:24 +0200

    be funks

--- script.js
+++ script.js
@@ -5,24 +5,34 @@ const kmiDOM = document.querySelector(".kmi");
 const btnscaicioDOM = document.querySelector(".btnscaicio");
 const btnclearDOM = document.querySelector(".clear");
 
+
+
+
 function countKmi() {
     const ugi = parseFloat(ugiDOM.value);
     const svori = parseFloat(svoriDOM.value);
     
-    let result = (svori / (ugi * ugi))*10000;
+    let result = ((svori / (ugi * ugi))*10000).toFixed(2);
         
     let kmiClass = ' ';
-           
-    if(result > 0 && result < 18.5){
+    if (ugiDOM === " " || svoriDOM === " "){
+        table2DOM.innerHTML =  `<div id = "kmi" class="kmi">
+        <p>Pateikite tinkamą  aukštį ir svoti!.</p> </div>`
+
+    }
+    
+    else if (isNaN(parseFloat(ugiDOM.value)) || isNaN(parseFloat(svoriDOM.value))) {
+        table2DOM.innerHTML = `<div id="kmi" class="kmi">
+            <p>Pateikite galiojantį aukštį ir svorį!</p>
+        </div>`;
+    }else{
+     if(result > 0 && result < 18.5){
         kmiClass = "violet";
-    }
-    if(result > 18.5 && result < 25){
+    }else if(result > 18.5 && result < 25){
         kmiClass = "green";
-    }
-    if(result > 25 && result < 30){
+    }else if(result > 25 && result < 30){
         kmiClass = "blue";
-    }
-    if(result > 30 && result < 43){
+    }else if(result > 30 && result < Infinity){
         kmiClass = "red";
     }
    
@@ -31,11 +41,13 @@ function countKmi() {
             </div>`;
             table2DOM.innerHTML = HTML; 
 }
-
+}
 btnscaicioDOM.addEventListener("click", countKmi);
 
 const clear = () => {
     table2DOM.innerHTML = `<div class="kmi"></div>`;
+    document.querySelector(".ugi input").value = " "
+    document.querySelector(".svori input").value = " "
     
 };
 btnclearDOM.addEventListener("click", clear);
